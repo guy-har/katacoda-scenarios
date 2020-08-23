85c18dbc53ab9a82ce8370556efccb0edafcc391
guyhardonag <guy.hardonag@treeverse.io>
2020-08-23 17:20:00 +0300

    test add spinner

--- test-scenario/background.sh
+++ test-scenario/background.sh
@@ -3,15 +3,48 @@ touch ~/test.txt
 export LAKEFS_STATS_ENABLED=false
 mkdir ~/lakefs
 
-wget  https://raw.githubusercontent.com/treeverse/lakeFS/master/docker-compose.yaml -P /home/lakefs/
-cd /home/lakefs/
-echo "done" >> /root/katacoda-mid
-TAG=latest LAKEFS_BLOCKSTORE_TYPE=local docker-compose up -d
-echo "done" >> /root/katacoda-up
-docker-compose exec lakefs wait-for localhost:8000 -- echo "done" >> /root/katacoda-done-waiting ;
-sleep 2;
-echo "done" >> /root/katacoda-finished ;
-docker-compose exec -e LAKEFS_LOGGING_LEVEL=ERROR lakefs  sh -c 'lakefs init --user-name demo | tail -3 > /home/lakefs/.lakectl.yaml' ;
-docker-compose exec lakefs sh -c 'echo -e "server:\n  endpoint_url: http://localhost:8000/api/v1\n" >> /home/lakefs/.lakectl.yaml' ;
+cat >lakefs/setup-lakefs.sh<<EOS
+#!/bin/bash
+wait-for localhost:8000
+LAKEFS_LOGGING_LEVEL=ERROR lakefs init --user-name demo | tail -3 > /home/lakefs/.lakectl.yaml
+echo -e "server:\n  endpoint_url: http://localhost:8000/api/v1\n" >> /home/lakefs/.lakectl.yaml
+EOS
+chmod +x lakefs/setup-lakefs.sh
+
+cat > /home/lakefs/docker-compose.yaml <<<EOS
+version: '3'
+services:
+  lakefs:
+    image: "treeverse/lakefs:latest"
+    ports:
+      - 8000:8000
+    depends_on:
+      - postgres
+    volumes:
+      - lakefs:/home/lakefs
+    environment:
+      LAKEFS_AUTH_ENCRYPT_SECRET_KEY: some random secret string
+      LAKEFS_DATABASE_CONNECTION_STRING: postgres://lakefs:lakefs@postgres/postgres?sslmode=disable
+      LAKEFS_BLOCKSTORE_TYPE: local
+      LAKEFS_BLOCKSTORE_LOCAL_PATH: /home/lakefs
+      LAKEFS_GATEWAYS_S3_DOMAIN_NAME: s3.local.lakefs.io:8000
+    entrypoint: ["/app/wait-for", "postgres:5432", "--", "/app/lakefs", "run"]
+  postgres:
+    image: "postgres:11"
+    environment:
+      POSTGRES_USER: lakefs
+      POSTGRES_PASSWORD: lakefs
+  lakefs-setup:
+    image: "treeverse/lakefs:latest"
+    command: /home/lakefs/setup-lakefs.sh
+    depends_on:
+      - lakefs
+    volumes:
+      - lakefs:/home/lakefs
+EOS
+
+
+echo "done" >> /root/katacoda-finished
+docker-compose up -d
 echo "done" >> /root/katacoda-background-finished
 
