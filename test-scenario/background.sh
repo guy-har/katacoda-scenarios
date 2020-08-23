c6a4e5861a16bd233f4a0c32cd260c93cda599dd
guyhardonag <guy.hardonag@treeverse.io>
2020-08-23 14:49:42 +0300

    add tag to lakefs docker

--- test-scenario/background.sh
+++ test-scenario/background.sh
@@ -4,7 +4,7 @@ mkdir ~/lakefs
 
 wget  https://raw.githubusercontent.com/treeverse/lakeFS/master/docker-compose.yaml -P /home/lakefs/
 cd /home/lakefs/
-docker-compose up -d
+TAG=latest docker-compose up -d
 docker-compose exec lakefs wait-for localhost:8000
 docker-compose exec -e LAKEFS_LOGGING_LEVEL=ERROR lakefs  sh -c 'lakefs init --user-name demo | tail -3 > /home/lakefs/.lakectl.yaml'
 docker-compose exec lakefs sh -c 'echo -e "server:\n  endpoint_url: http://localhost:8000/api/v1\n" >> /home/lakefs/.lakectl.yaml'
