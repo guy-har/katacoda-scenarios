4d25f1939071b8415ffc28b1de7ee1eb88bd35c0
guyhardonag <guy.hardonag@treeverse.io>
2020-08-23 14:40:03 +0300

    bugfix

--- test-scenario/background.sh
+++ test-scenario/background.sh
@@ -3,7 +3,7 @@ export LAKEFS_STATS_ENABLED=false
 mkdir ~/lakefs
 
 wget  https://raw.githubusercontent.com/treeverse/lakeFS/master/docker-compose.yaml -P /home/lakefs/
-cd home/lakefs/
+cd /home/lakefs/
 docker-compose up -d
 docker-compose exec lakefs wait-for localhost:8000
 docker-compose exec -e LAKEFS_LOGGING_LEVEL=ERROR lakefs  sh -c 'lakefs init --user-name demo | tail -3 > /home/lakefs/.lakectl.yaml'
