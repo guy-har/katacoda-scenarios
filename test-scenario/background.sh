3a6dcc40f455a2dc0bced75c8413ee737517060f
guyhardonag <guy.hardonag@treeverse.io>
2020-08-23 16:27:34 +0300

    test add spinner

--- test-scenario/background.sh
+++ test-scenario/background.sh
@@ -10,7 +10,7 @@ TAG=latest LAKEFS_BLOCKSTORE_TYPE=local docker-compose up -d
 echo "done" >> /root/katacoda-up
 docker-compose exec lakefs wait-for localhost:8000
 echo "done" >> /root/katacoda-finished
-docker-compose exec -e LAKEFS_LOGGING_LEVEL=ERROR lakefs  sh -c 'lakefs init --user-name demo | tail -3 > /home/lakefs/.lakectl.yaml' && \
-docker-compose exec lakefs sh -c 'echo -e "server:\n  endpoint_url: http://localhost:8000/api/v1\n" >> /home/lakefs/.lakectl.yaml' && \
+docker-compose exec -e LAKEFS_LOGGING_LEVEL=ERROR lakefs  sh -c 'lakefs init --user-name demo | tail -3 > /home/lakefs/.lakectl.yaml'
+docker-compose exec lakefs sh -c 'echo -e "server:\n  endpoint_url: http://localhost:8000/api/v1\n" >> /home/lakefs/.lakectl.yaml'
 echo "done" >> /root/katacoda-background-finished
 
