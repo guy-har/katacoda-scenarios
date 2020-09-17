76e779f9fbcaedf0785ceea65eb334a6a4259366
guyhardonag <guy.hardonag@treeverse.io>
2020-09-17 15:13:35 +0300

    testing add spark to spark scenario

--- sparkandmerge/foreground.sh
+++ sparkandmerge/foreground.sh
@@ -1,5 +1,5 @@
 wait 2
 kataclient
-docker pull bitnami/spark
-docker run -d --name spark bitnami/spark
+#docker pull bitnami/spark
+#docker run -d --name spark bitnami/spark
 #docker exec spark spark-shell
