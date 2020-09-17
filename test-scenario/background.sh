68ea9ba59debc007c4c3bcbf68968ffdfb536f09
guyhardonag <guy.hardonag@treeverse.io>
2020-09-17 15:45:16 +0300

    testing add spark to spark scenario

--- test-scenario/background.sh
+++ test-scenario/background.sh
@@ -1,3 +1,3 @@
 #sleep 1; wait.sh && docker-compose exec lakefs sh
-apt update && apt install -y wget
-wget https://datasets.imdbws.com/name.basics.tsv.gz
+#apt update && apt install -y wget
+#wget https://datasets.imdbws.com/name.basics.tsv.gz
